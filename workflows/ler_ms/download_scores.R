ae03162265b588476cf57468cedc3dbfaad5badd
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-07-11 19:47:20 +0000

    remove forecast download

--- workflows/ler_ms/download_scores.R
+++ workflows/ler_ms/download_scores.R
@@ -1,11 +1,8 @@
 lake_directory <- here::here()
+
 # download.file(url = "https://zenodo.org/record/7951402/files/scores.zip?download=1",
          # destfile = file.path(lake_directory,"scores.zip"), method = "curl")
 unzip(file.path(lake_directory,"scores.zip"))
-
-# download.file(url = "https://zenodo.org/record/7951402/files/forecasts.zip?download=1",
-         # destfile = file.path(lake_directory,"forecasts.zip"), method = "curl")
-unzip(file.path(lake_directory,"forecasts.zip"))
 
 # download.file(url = "https://zenodo.org/record/7951402/files/targets.zip?download=1",
          # destfile = file.path(lake_directory,"targets.zip"), method = "curl")
