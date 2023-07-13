3198ebc423271cf978753429d0b36de655a04c90
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-07-13 17:07:36 -0400

    typo

--- workflows/ler_ms/download_scores.R
+++ workflows/ler_ms/download_scores.R
@@ -1,6 +1,6 @@
 lake_directory <- here::here()
 
-download.file(url = "https://sandbox.zenodo.org/record/1221469/files/targets.zip?download=1",
+download.file(url = "https://sandbox.zenodo.org/record/1221469/files/scores.zip?download=1",
               destfile = file.path(lake_directory,"scores.zip"), method = "curl")
 unzip(file.path(lake_directory,"scores.zip"))
 
